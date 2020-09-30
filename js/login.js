9cf50851c338445354702480fd0f963952c90a29
David Logic <58322362+DavidLogic@users.noreply.github.com>
2020-09-30 22:13:31 +0800

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,34 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码
+    if(userName=="李思雯" &&  pwd=="1224"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			requestFullScreen();
+			setTimeout(function(){
+				location.href="BirthdayCake.html";
+			},2000);
+		}
+	else{
+		alert("你可能输错了密码！");
+	}
+});
+
+function requestFullScreen(element) {
+	var element=document.documentElement;
+	var requestMethod = element.requestFullScreen || //W3C
+	element.webkitRequestFullScreen || //Chrome等
+	element.mozRequestFullScreen || //FireFox
+	element.msRequestFullScreen; //IE11
+	if (requestMethod) {
+		requestMethod.call(element);
+	}
+	else if (typeof window.ActiveXObject !== "undefined") {//for Internet Explorer
+		var wscript = new ActiveXObject("WScript.Shell");
+		if (wscript !== null) {
+		 wscript.SendKeys("{F11}");
+		}
+	}
+}
